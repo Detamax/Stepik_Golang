f2d2c2d9b5f05f68be8fb2ea254bc96656b1ca56
Detamax <44142583+Detamax@users.noreply.github.com>
2022-02-23 17:06:18 +0300

    Create stepik-1.10.2.go

---
+++ 1.10/stepik-1.10.2.go
@@ -0,0 +1,31 @@
+/*
+
+Требуется написать программу, при выполнении которой с клавиатуры считываются два натуральных числа A и B (каждое не более 100, A < B). Вывести сумму всех чисел от A до B  включительно.
+
+Sample Input:
+
+1 5
+Sample Output:
+
+15
+
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a, b int
+		sum int = 0
+		i int
+		)
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	for i = a; i <=b; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+}
