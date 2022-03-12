0e1e3407d085b070ed759af4a49f85ec5b36d681
Detamax <44142583+Detamax@users.noreply.github.com>
2022-03-12 20:17:35 +0300

    Create 1.10.5.go

---
+++ 1.10/1.10.5.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+    var n,c,d int
+    fmt.Scan(&n,&c,&d)
+    for i:=c; i<=n; i+=c{
+        if i%d!=0 {
+            fmt.Print(i)
+            return
+        }
+    }
+}
